d7df59f25f87844d44bd42877302c174812552cf
Everest Munro <evmunro1@gmail.com>
2024-11-09 20:38:53 -0800

    Add options to manage output size and pagination

--- cmd/llmcat/main.go
+++ cmd/llmcat/main.go
@@ -10,6 +10,8 @@ import (
 
 func main() {
 	var options llmcat.RenderFileOptions
+	var dirOptions llmcat.RenderDirectoryOptions
+
 	var rootCmd = &cobra.Command{
 		Use:   "llmcat [path]",
 		Short: "Display file contents with optional line numbers and formatting",
@@ -23,10 +25,9 @@ func main() {
 			}
 
 			if fileInfo.IsDir() {
-				dirOptions := &llmcat.RenderDirectoryOptions{
-					FileOptions: &options,
-				}
-				output, err := llmcat.RenderDirectory(path, dirOptions)
+				dirOptions.FileOptions = &options
+
+				output, err := llmcat.RenderDirectory(path, &dirOptions)
 				if err != nil {
 					return fmt.Errorf("error processing directory: %v", err)
 				}
@@ -46,9 +47,21 @@ func main() {
 
 	// Add flags
 	flags := rootCmd.Flags()
+
+	// File rendering flags
 	flags.BoolVarP(&options.OutputMarkdown, "markdown", "m", true, "output in markdown format")
 	flags.BoolVarP(&options.ShowLineNumbers, "line-numbers", "n", true, "show line numbers")
 	flags.StringVarP(&options.GutterSeparator, "separator", "s", "|", "gutter separator character")
+
+	// Pagination flags
+	flags.IntVarP(&options.PageSize, "page-size", "p", 10000, "number of lines to show (0 = show all)")
+	flags.IntVar(&options.StartLine, "start-line", 1, "first line to show (1-based)")
+	flags.BoolVar(&options.ShowPageInfo, "show-page-info", true, "show page information in header")
+
+	// Directory flags
+	flags.StringSliceVarP(&dirOptions.IgnoreGlobs, "ignore", "i", []string{"**/.git/**"}, "glob patterns to ignore")
+	// flags.StringVarP(&excludeExtsString, "exclude-ext", "e", "", "comma-separated list of file extensions to exclude")
+	// flags.BoolVarP(&dirOptions.ShowTree, "tree", "t", false, "show directory tree")
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
--- llmcat.go
+++ llmcat.go
@@ -14,13 +14,22 @@ type RenderFileOptions struct {
 	OutputMarkdown  bool   `json:"output_markdown"`
 	ShowLineNumbers bool   `json:"hide_line_numbers"`
 	GutterSeparator string `json:"gutter_separator"`
+	PageSize        int    `json:"page_size"`
+	StartLine       int    `json:"start_line"`
+	ShowPageInfo    bool   `json:"show_page_info"`
 }
 
 func (ro *RenderFileOptions) SetDefaults() {
 	if ro.GutterSeparator == "" {
-		gs := "|"
+		ro.GutterSeparator = "|"
+	}
 
-		ro.GutterSeparator = gs
+	if ro.PageSize == 0 {
+		ro.PageSize = 10000
+	}
+
+	if ro.StartLine < 1 {
+		ro.StartLine = 1
 	}
 }
 
@@ -29,22 +38,63 @@ func RenderFile(filename, text string, options *RenderFileOptions) string {
 
 	options.SetDefaults()
 
-	if options.OutputMarkdown {
-		outputLines = append(outputLines, "```"+filename)
-	}
-
 	lines := strings.Split(text, "\n")
+	totalLines := len(lines)
 
 	gutterWidth := len(fmt.Sprint(len(lines))) + 1 // add 1 line for a space before the separator
 
-	for i, line := range lines {
+	// Calculate page bounds
+	startIndex := options.StartLine - 1
+	endIndex := totalLines
+
+	if options.PageSize > 0 {
+		endIndex = startIndex + options.PageSize
+		if endIndex > totalLines {
+			endIndex = totalLines
+		}
+	}
+
+	// Validate bounds
+	if startIndex >= totalLines {
+		startIndex = totalLines - 1
+		if startIndex < 0 {
+			startIndex = 0
+		}
+		endIndex = totalLines
+	}
+
+	if options.OutputMarkdown {
+		header := fmt.Sprintf("```%s", filename)
+		if options.ShowPageInfo && options.PageSize > 0 {
+			header += fmt.Sprintf(" (Lines %d-%d of %d)", startIndex+1, endIndex, totalLines)
+		}
+		outputLines = append(outputLines, header)
+	}
+
+	if startIndex > 0 {
+		marker := fmt.Sprintf("... (%d lines above) ...", startIndex)
 		if options.ShowLineNumbers {
-			lineNum := i + 1
+			marker = fmt.Sprintf("%s%s %s", strings.Repeat(" ", gutterWidth), options.GutterSeparator, marker)
+		}
+		outputLines = append(outputLines, marker)
+	}
+
+	for i, line := range lines[startIndex:endIndex] {
+		if options.ShowLineNumbers {
+			lineNum := i + startIndex + 1
 			padding := strings.Repeat(" ", gutterWidth-len(fmt.Sprint(lineNum)))
 			line = fmt.Sprintf("%d%s%s %s", lineNum, padding, options.GutterSeparator, line)
 		}
 
 		outputLines = append(outputLines, line)
+	}
+
+	if endIndex < totalLines {
+		marker := fmt.Sprintf("... (%d lines below) ...", totalLines-endIndex)
+		if options.ShowLineNumbers {
+			marker = fmt.Sprintf("%s%s %s", strings.Repeat(" ", gutterWidth), options.GutterSeparator, marker)
+		}
+		outputLines = append(outputLines, marker)
 	}
 
 	if options.OutputMarkdown {
