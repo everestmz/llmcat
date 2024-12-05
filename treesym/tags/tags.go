675f20571cac8da7e29de348db841e7b89af6d48
Everest Munro <evmunro1@gmail.com>
2024-12-04 23:23:00 -0800

    Move language tags to separate embedded files

--- treesym/tags/tags.go
+++ treesym/tags/tags.go
@@ -1,392 +1,52 @@
 package tags
 
 import (
+	_ "embed"
 	"fmt"
 
 	"github.com/everestmz/llmcat/treesym/language"
 )
 
+var (
+	//go:embed c.scm
+	CTags string
+
+	//go:embed cpp.scm
+	CppTags string
+
+	//go:embed go.scm
+    GoTags string
+
+	//go:embed python.scm
+	PythonTags string
+
+	//go:embed ruby.scm
+	RubyTags string
+
+	//go:embed rust.scm
+	RustTags string
+
+	//go:embed typescript.scm
+	TypescriptTags string
+
+	//go:embed java.scm
+	JavaTags string
+
+	//go:embed javascript.scm
+    JavascriptTags string
+)
+
 var queries = map[language.Language]string{
-	language.Python: `(class_definition
-  name: (identifier) @name.definition.class) @definition.class
-
-(function_definition
-  name: (identifier) @name.definition.function) @definition.function
-
-(call
-  function: [
-      (identifier) @name.reference.call
-      (attribute
-        attribute: (identifier) @name.reference.call)
-  ]) @reference.call`,
-	language.Javascript: `(
-  (comment)* @doc
-  .
-  (method_definition
-    name: (property_identifier) @name.definition.method) @definition.method
-  (#not-eq? @name.definition.method "constructor")
-  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
-  (#select-adjacent! @doc @definition.method)
-)
-
-(
-  (comment)* @doc
-  .
-  [
-    (class
-      name: (_) @name.definition.class)
-    (class_declaration
-      name: (_) @name.definition.class)
-  ] @definition.class
-  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
-  (#select-adjacent! @doc @definition.class)
-)
-
-(
-  (comment)* @doc
-  .
-  [
-    (function
-      name: (identifier) @name.definition.function)
-    (function_declaration
-      name: (identifier) @name.definition.function)
-    (generator_function
-      name: (identifier) @name.definition.function)
-    (generator_function_declaration
-      name: (identifier) @name.definition.function)
-  ] @definition.function
-  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
-  (#select-adjacent! @doc @definition.function)
-)
-
-(
-  (comment)* @doc
-  .
-  (lexical_declaration
-    (variable_declarator
-      name: (identifier) @name.definition.function
-      value: [(arrow_function) (function)]) @definition.function)
-  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
-  (#select-adjacent! @doc @definition.function)
-)
-
-(
-  (comment)* @doc
-  .
-  (variable_declaration
-    (variable_declarator
-      name: (identifier) @name.definition.function
-      value: [(arrow_function) (function)]) @definition.function)
-  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
-  (#select-adjacent! @doc @definition.function)
-)
-
-(assignment_expression
-  left: [
-    (identifier) @name.definition.function
-    (member_expression
-      property: (property_identifier) @name.definition.function)
-  ]
-  right: [(arrow_function) (function)]
-) @definition.function
-
-(pair
-  key: (property_identifier) @name.definition.function
-  value: [(arrow_function) (function)]) @definition.function
-
-(
-  (call_expression
-    function: (identifier) @name.reference.call) @reference.call
-  (#not-match? @name.reference.call "^(require)$")
-)
-
-(call_expression
-  function: (member_expression
-    property: (property_identifier) @name.reference.call)
-  arguments: (_) @reference.call)
-
-(new_expression
-  constructor: (_) @name.reference.class) @reference.class`,
-	language.C: `(struct_specifier name: (type_identifier) @name.definition.class body:(_)) @definition.class
-
-(declaration type: (union_specifier name: (type_identifier) @name.definition.class)) @definition.class
-
-(function_declarator declarator: (identifier) @name.definition.function) @definition.function
-
-(type_definition declarator: (type_identifier) @name.definition.type) @definition.type
-
-(enum_specifier name: (type_identifier) @name.definition.type) @definition.type`,
-	language.Typescript: `(function_signature
-  name: (identifier) @name.definition.function) @definition.function
-
-(method_signature
-  name: (property_identifier) @name.definition.method) @definition.method
-
-(abstract_method_signature
-  name: (property_identifier) @name.definition.method) @definition.method
-
-(abstract_class_declaration
-  name: (type_identifier) @name.definition.class) @definition.class
-
-(module
-  name: (identifier) @name.definition.module) @definition.module
-
-(interface_declaration
-  name: (type_identifier) @name.definition.interface) @definition.interface
-
-(type_annotation
-  (type_identifier) @name.reference.type) @reference.type
-
-(new_expression
-  constructor: (identifier) @name.reference.class) @reference.class
-
-(function_declaration
-  name: (identifier) @name.definition.function) @definition.function
-
-(method_definition
-  name: (property_identifier) @name.definition.method) @definition.method
-
-(class_declaration
-  name: (type_identifier) @name.definition.class) @definition.class
-
-(interface_declaration
-  name: (type_identifier) @name.definition.class) @definition.class
-
-(type_alias_declaration
-  name: (type_identifier) @name.definition.type) @definition.type
-
-(enum_declaration
-  name: (identifier) @name.definition.enum) @definition.enum`,
-	language.Tsx: `(function_signature
-  name: (identifier) @name.definition.function) @definition.function
-
-(method_signature
-  name: (property_identifier) @name.definition.method) @definition.method
-
-(abstract_method_signature
-  name: (property_identifier) @name.definition.method) @definition.method
-
-(abstract_class_declaration
-  name: (type_identifier) @name.definition.class) @definition.class
-
-(module
-  name: (identifier) @name.definition.module) @definition.module
-
-(interface_declaration
-  name: (type_identifier) @name.definition.interface) @definition.interface
-
-(type_annotation
-  (type_identifier) @name.reference.type) @reference.type
-
-(new_expression
-  constructor: (identifier) @name.reference.class) @reference.class
-
-(function_declaration
-  name: (identifier) @name.definition.function) @definition.function
-
-(method_definition
-  name: (property_identifier) @name.definition.method) @definition.method
-
-(class_declaration
-  name: (type_identifier) @name.definition.class) @definition.class
-
-(interface_declaration
-  name: (type_identifier) @name.definition.class) @definition.class
-
-(type_alias_declaration
-  name: (type_identifier) @name.definition.type) @definition.type
-
-(enum_declaration
-  name: (identifier) @name.definition.enum) @definition.enum`,
-	language.Go: `(
-  (comment)* @doc
-  .
-  (function_declaration
-    name: (identifier) @name.definition.function) @definition.function
-  (#strip! @doc "^//\\s*")
-  (#set-adjacent! @doc @definition.function)
-)
-
-(
-  (comment)* @doc
-  .
-  (method_declaration
-    name: (field_identifier) @name.definition.method) @definition.method
-  (#strip! @doc "^//\\s*")
-  (#set-adjacent! @doc @definition.method)
-)
-
-(call_expression
-  function: [
-    (identifier) @name.reference.call
-    (parenthesized_expression (identifier) @name.reference.call)
-    (selector_expression field: (field_identifier) @name.reference.call)
-    (parenthesized_expression (selector_expression field: (field_identifier) @name.reference.call))
-  ]) @reference.call
-
-(type_spec
-  name: (type_identifier) @name.definition.type) @definition.type
-
-(type_identifier) @name.reference.type @reference.type`,
-	language.Rust: `; ADT definitions
-
-(struct_item
-    name: (type_identifier) @name.definition.class) @definition.class
-
-(enum_item
-    name: (type_identifier) @name.definition.class) @definition.class
-
-(union_item
-    name: (type_identifier) @name.definition.class) @definition.class
-
-; type aliases
-
-(type_item
-    name: (type_identifier) @name.definition.class) @definition.class
-
-; method definitions
-
-(declaration_list
-    (function_item
-        name: (identifier) @name.definition.method)) @definition.method
-
-; function definitions
-
-(function_item
-    name: (identifier) @name.definition.function) @definition.function
-
-; trait definitions
-(trait_item
-    name: (type_identifier) @name.definition.interface) @definition.interface
-
-; module definitions
-(mod_item
-    name: (identifier) @name.definition.module) @definition.module
-
-; macro definitions
-
-(macro_definition
-    name: (identifier) @name.definition.macro) @definition.macro
-
-; references
-
-(call_expression
-    function: (identifier) @name.reference.call) @reference.call
-
-(call_expression
-    function: (field_expression
-        field: (field_identifier) @name.reference.call)) @reference.call
-
-(macro_invocation
-    macro: (identifier) @name.reference.call) @reference.call
-
-; implementations
-
-(impl_item
-    trait: (type_identifier) @name.reference.implementation) @reference.implementation
-
-(impl_item
-    type: (type_identifier) @name.reference.implementation
-    !trait) @reference.implementation`,
-	language.Cpp: `(struct_specifier name: (type_identifier) @name.definition.class body:(_)) @definition.class
-
-(declaration type: (union_specifier name: (type_identifier) @name.definition.class)) @definition.class
-
-(function_declarator declarator: (identifier) @name.definition.function) @definition.function
-
-(function_declarator declarator: (field_identifier) @name.definition.function) @definition.function
-
-(function_declarator declarator: (qualified_identifier scope: (namespace_identifier) @scope name: (identifier) @name.definition.method)) @definition.method
-
-(type_definition declarator: (type_identifier) @name.definition.type) @definition.type
-
-(enum_specifier name: (type_identifier) @name.definition.type) @definition.type
-
-(class_specifier name: (type_identifier) @name.definition.class) @definition.class`,
-	language.Ruby: `; Method definitions
-
-(
-  (comment)* @doc
-  .
-  [
-    (method
-      name: (_) @name.definition.method) @definition.method
-    (singleton_method
-      name: (_) @name.definition.method) @definition.method
-  ]
-  (#strip! @doc "^#\\s*")
-  (#select-adjacent! @doc @definition.method)
-)
-
-(alias
-  name: (_) @name.definition.method) @definition.method
-
-(setter
-  (identifier) @ignore)
-
-; Class definitions
-
-(
-  (comment)* @doc
-  .
-  [
-    (class
-      name: [
-        (constant) @name.definition.class
-        (scope_resolution
-          name: (_) @name.definition.class)
-      ]) @definition.class
-    (singleton_class
-      value: [
-        (constant) @name.definition.class
-        (scope_resolution
-          name: (_) @name.definition.class)
-      ]) @definition.class
-  ]
-  (#strip! @doc "^#\\s*")
-  (#select-adjacent! @doc @definition.class)
-)
-
-; Module definitions
-
-(
-  (module
-    name: [
-      (constant) @name.definition.module
-      (scope_resolution
-        name: (_) @name.definition.module)
-    ]) @definition.module
-)
-
-; Calls
-
-(call method: (identifier) @name.reference.call) @reference.call
-
-(
-  [(identifier) (constant)] @name.reference.call @reference.call
-  (#is-not? local)
-  (#not-match? @name.reference.call "^(lambda|load|require|require_relative|__FILE__|__LINE__)$")
-)`,
-	language.Java: `(class_declaration
-  name: (identifier) @name.definition.class) @definition.class
-
-(method_declaration
-  name: (identifier) @name.definition.method) @definition.method
-
-(method_invocation
-  name: (identifier) @name.reference.call
-  arguments: (argument_list) @reference.call)
-
-(interface_declaration
-  name: (identifier) @name.definition.interface) @definition.interface
-
-(type_list
-  (type_identifier) @name.reference.implementation) @reference.implementation
-
-(object_creation_expression
-  type: (type_identifier) @name.reference.class) @reference.class
-
-(superclass (type_identifier) @name.reference.class) @reference.class`,
+	language.Python: PythonTags,
+	language.Javascript: JavascriptTags,
+	language.C: CTags,
+	language.Typescript: TypescriptTags,
+	language.Tsx: TypescriptTags,
+	language.Go: GoTags,
+	language.Rust: RustTags,
+	language.Cpp: CppTags,
+	language.Ruby: RubyTags,
+	language.Java: JavaTags,
 }
 
 func GetTagsQuery(lang language.Language) (string, error) {
